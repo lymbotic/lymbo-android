27335cd9ece47a67692c3b40ea0ab956d303d0ad
Florian Schwanz <florian.schwanz@interoberlin.de>
2017-10-08 17:58:07 +0200

    Made application no singleton

--- app/src/main/java/de/interoberlin/lymbo/App.kt
+++ app/src/main/java/de/interoberlin/lymbo/App.kt
@@ -1,13 +1,15 @@
 package de.interoberlin.lymbo
 
 import android.app.Application
+import android.content.Context
 
 class App : Application() {
-    private object Holder {
-        val INSTANCE = App()
+    companion object {
+        lateinit var context: Context
     }
 
-    companion object {
-        val instance: App by lazy { Holder.INSTANCE }
+    override fun onCreate() {
+        super.onCreate()
+        context = this
     }
 }
